5cf574761818edbc982e67561be62ab4a98ff5c5
Bastien Daniel <bastien.daniel@rawfish.com>
2023-08-04 11:01:46 +0200

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,31 @@ import Dropdown, { Option } from "./components/Dropdown/Dropdown";
 const options: Option[] = [
   { value: "option1", label: "Option 1" },
   { value: "option2", label: "Option 2" },
-  // Your other options here
 ];
+
 export default function App() {
   return (
     <>
-      <Dropdown
-        options={options}
-        onOptionChange={(option) => console.log(option)}
-      />
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <div>
+          <Dropdown
+            options={options}
+            // onOptionChange={(option) => console.log(option)}
+            onOptionChange={(option) =>
+              console.log("Dropdown 1: " + option.label)
+            }
+          />
+        </div>
+        <div>
+          <Dropdown
+            options={options}
+            // onOptionChange={(option) => console.log(option)}
+            onOptionChange={(option) =>
+              console.log("Dropdown 2: " + option.label)
+            }
+          />
+        </div>
+      </div>
     </>
   );
 }
